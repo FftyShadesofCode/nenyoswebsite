fe438f0b0c171131dcf6a6926b80efa9da1e4a76
Philip R McDavid <31264591+FftyShadesofCode@users.noreply.github.com>
2022-03-20 19:42:03 -0400

    Fixing the Uploader

--- client/src/components/form/form.js
+++ client/src/components/form/form.js
@@ -1,55 +1,103 @@
-import React from "react";
-import cl from "./formStyle.module.scss";
+import {jsonPost, jsonGet, jsonPatch, jsonDelete} from './Ajax';
 
-export default function ({title,submitFormHandler, state, disabled, checkValidity, onChangehandler, children}) {
-    return (
-        <div className={cl.container}>
-            <form onSubmit={submitFormHandler} className={cl.form}>
-                <h1 align="center">{title}</h1>
-                {Object.entries(state.formData).map((item, index) => {
-                    return <div
-                        style={item[1].elementConfig.type === "checkbox" ?{display:"flex",flexDirection:"row", justifyContent:"center", alignItems:"flex-start"}:null}
-                        key={index}
-                        className={cl["form-wrapper"]}>
-                        <label
-                            //style={item[1].elementConfig.type === "checkbox" ?{display:"inline-flex", width:"4em"}:null}
-                        >
-                            {item[1].label}
-                        </label>
-                        {item[1].elementType === "input" ?
-                            <input
-                                onBlur={checkValidity}
-                                onChange={onChangehandler} name={item[0]}
-                                value={item[1].values}
-                                type={item[1].elementConfig.type}
-                                placeholder={item[1].elementConfig.placeholder}/>
-                            : item[1].elementType === "select" ?
-                                <select name={item[0]}
-                                        onChange={onChangehandler}
-                                        onBlur={checkValidity}
-                                        value={item[1].value}
-                                >
-                                    {item[1].elementConfig.option.map((option) => {
-                                        return <option key={option.value} value={option.value}>{option.displayValue}</option>
-                                    })}
-                                </select>
-                                : item[1].elementType === "checkbox" ?
-                                    <input
-                                        style={{display: "inline-block", width:"2em"}}
-                                        onBlur={checkValidity}
-                                        onChange={onChangehandler} name={item[0]}
-                                        checked={item[1].checked}
-                                        value={item[1].values}
-                                        type={item[1].elementConfig.type}
-                                        placeholder={item[1].elementConfig.placeholder}/>
-                                    : null
-                        }
-                        <span className={cl.error}>{!item[1].isValid ? item[1].errorMsg : null}</span>
-                    </div>
-                })}
-                {children}
-                <button  className={state.completeFormValidity? cl["submit-button"]: cl["submit-button-disabled"]}>Submit</button>
-            </form>
-        </div>
-    );
+export class Form {
+    url;
+    fields;
+    setFields;
+    success;
+    method;
+
+    constructor(url, fields, setFields, success, method = 'POST') {
+        this.url = url;
+        this.fields = fields;
+        this.setFields = setFields;
+        this.success = success;
+        this.method = method;
+    }
+
+    methodMap = {POST: jsonPost, GET: jsonGet, PATCH: jsonPatch, DELETE: jsonDelete};
+
+    setUrl = (url) => {
+        this.url = url;
+    }
+
+    setMethod = (method) => {
+        this.method = method;
+    }
+
+    handleInputChanges = (evt) => {
+        const key = evt.target.name;
+        const value = evt.target.value;
+        const newFields = this.fields;
+        newFields[key].value = value;
+        this.setFields({...newFields});
+    }
+
+    submitForm = async(evt) => {
+        evt.preventDefault();
+        const disabled = evt.target.getAttribute('disabled');
+        if(disabled && (disabled === 'true' || disabled === true)) return;
+        evt.target.setAttribute('disabled', true);
+        this.clearFormErrors();
+        const data = this.prepareForPost();
+        const resp = await this.methodMap[this.method](this.url, data);
+        if(resp.success) {
+            this.clearFormErrors();
+            this.clearFormValues();
+            this.success(resp);
+        } else {
+            this.processFormErrors(resp);
+        }
+        evt.target.removeAttribute('disabled');
+    }
+
+    prepareForPost = () => {
+        const data = {};
+        for(const[key,value] of Object.entries(this.fields)) {
+            data[key] = value.value;
+        }
+        return data;
+    }
+
+    processFormErrors = (resp) => {
+        const newState = this.fields;
+        resp.errors.forEach(error => {
+            const key = error.path;
+            const msg = error.message;
+            if(newState.hasOwnProperty(key)){
+                newState[key].isInvalid = true;
+                newState[key].msg = msg;
+            }
+        })
+        this.setFields({...newState});
+    }
+
+    clearFormErrors = () => {
+        const newState = this.fields;
+        for(const[key] of Object.entries(this.fields)) {
+            newState[key].isInvalid = false;
+            newState[key].msg = "";
+        }
+        this.setFields({...newState});
+    }
+
+    clearFormValues = () => {
+        const newState = this.fields;
+        for(const[key] of Object.entries(this.fields)) {
+            newState[key].value = "";
+        }
+        this.setFields({...newState});
+    }
+
+    populateFormValues = (vals) => {
+        const newState = this.fields;
+        for(const[key] of Object.entries(this.fields)) {
+            if(vals.hasOwnProperty(key)) {
+                newState[key].value = vals[key];
+            }
+        }
+        this.setFields({...newState});
+    }
+
+
 }
--- client/src/pages/Uploader.js
+++ client/src/pages/Uploader.js
@@ -1,309 +1,90 @@
-import React from "react";
-import { Form, Field } from "folo-forms";
-import { Button, Label } from "reactstrap";
-import Dropzone from "../components/Dropzone";
-import cuid from "cuid";
+import React, {useState} from 'react'
+import { Form } from '../components/form/form'
+import Dropzone from "../components/Dropzone"
+import cuid from "cuid"
 
-import "../CSS Files/Uploader.css";
+import '../CSS Files/Uploader.css'
 import axios from "axios";
+import FieldBlock from "../components/form/FieldBlock";
 
-class Uploader extends React.Component {
-  state = {
-    name: {
-      firstName: "",
-      lastName: "",
-    },
-    orchid: {
-      orchidGenera: "",
-      orchidSpecies: "",
-      basicInformation: {
-        fragrant: "",
-        careInformation: {
-          water: "",
-          light: {
-            lightIntensity: "",
-            lightType: "",
-          },
-          temp: "",
-          humidity: "",
-        },
-        images: "",
-      },
-    },
-  };
+export function Uploader() {
+  // const state = {
+  //   name: {
+  //     firstName: '',
+  //     lastName: ''
+  //   },
+  //   orchid: {
+  //     orchidGenera: '',
+  //     orchidSpecies: '',
+  //     basicInformation: {
+  //       fragrant: '',
+  //       careInformation: {
+  //         water: '',
+  //         light: {
+  //           lightIntensity: '',
+  //           lightType: '',
+  //         },
+  //         temp: '',
+  //         humidity: '',
+  //       },
+  //       images: '',
+  //     }
+  //   }
+  // }
 
-  componentDidMount = () => {
-    this.getOrchidPost();
-  };
+  const [fields, setFields] = useState({
+    firstName: {value: '', isInvalid: false, msg: ''},
+    lastName: {value: '', isInvalid: false, msg: ''},
+    orchidGenera: {value: '', isInvalid: false, msg: ''},
+    orchidSpecies: {value: '', isInvalid: false, msg: ''}
+  })
 
-  getOrchidPost = () => {
-    axios
-      .get("/uploader")
-      .then((response) => {
-        const data = response.data;
-        this.setState({ posts: data });
-        console.log("In-Bloom has been Uploaded!");
-      })
-      .catch(() => {
-        alert("Error retrieving In-Bloom!");
-      });
-  };
+  async function success(res) {
+    console.log(res)
+  }
 
-  handleChange = (target) => {
-    const { name, value } = target;
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [orchidGenera, setOrchidGenera] = useState('')
+  const [orchidSpecies, setOrchidSpecies] = useState('')
 
-    this.state.setState({
-      [name]: value,
-    });
-  };
+  const form = new Form('/uploader', fields, setFields, success)
 
-  submit = (event) => {
-    event.preventDefault();
-
-    const payload = {
-      name: {
-        firstName: this.state.firstName,
-        lastName: this.state.lastName,
-      },
-      orchid: {
-        orchidGenera: this.state.orchidGenera,
-        orchidSpecies: this.state.orchidSpecies,
-        basicInformation: {
-          fragrant: this.state.fragrant,
-          careInformation: {
-            water: this.state.water,
-            light: {
-              lightIntensity: this.state.lightIntensity,
-              lightType: this.state.lightType,
-            },
-            temp: this.state.temp,
-            humidity: this.state.humidity,
-          },
-          images: this.state.images,
-        },
-      },
-    };
-
-    axios({
-      url: "/uploader",
-      method: "POST",
-      data: payload,
-    })
-      .then(() => {
-        console.log("Data has been sent to the server");
-        this.resetUserInputs();
-        this.getOrchidPost();
-      })
-      .catch(() => {
-        console.log("Internal server error");
-      });
-  };
-
-  resetUserInputs = () => {
-    this.setState({
-      name: {
-        firstName: "",
-        lastName: "",
-      },
-      orchid: {
-        orchidGenera: "",
-        orchidSpecies: "",
-        basicInformation: {
-          fragrant: "",
-          careInformation: {
-            water: "",
-            light: {
-              lightIntensity: "",
-              lightType: "",
-            },
-            temp: "",
-            humidity: "",
-          },
-          images: "",
-        },
-      },
-    });
-  };
-
-  render() {
     return (
-      <div className='form-container'>
         <div className='uploader-container'>
           <h3 className='header-one'>Orchids of NENYOS</h3>
-          <h4 className='header-two'>In-Bloom Uploader</h4>
-          <Form className='uploader-wrapper' onSubmit={this.submit}>
-              <div row={1} col={1}>
-                <Label>
-                  {" "}
-                  First Name
-                  <Field
-                    valueKey='firstName'
-                    type='text'
-                    placeholder='First Name'
-                    value={this.state.firstName}
-                    onChange={this.handleChange}
-                  />
-                </Label>
-              </div>
+          <h4 className='header-two'>Uploader</h4>
+          <div className='uploader-wrapper'>
+            <form >
+              <div className='input-container'>
+                <div className='name'>
+                  <FieldBlock id='lastName' value={fields.lastName.value} onChange={form.handleInputChanges} label='Last Name' isInvalid={fields.lastName.isInvalid} feedback={fields.lastName.msg} />
 
-              <div row={1} col={2}>
-                <Label>
-                  Last Name
-                  <Field
-                    valueKey='lastName'
-                    type='text'
-                    placeholder='Last Name'
-                    value={this.state.lastName}
-                    onChange={this.handleChange}
-                  />
-                </Label>
-              </div>
+                  <FieldBlock id='firstName' value={fields.firstName.value} onChange={form.handleInputChanges} label='First Name' isInvalid={fields.firstName.isInvalid} feedback={fields.firstName.msg} />
+                </div>
 
-              <div row={2} col={1}>
-                <Label>
-                  Orchid Genera
-                  <Field
-                    valueKey='orchidGenera'
-                    type='text'
-                    placeholder='Orchid Genera'
-                    value={this.state.orchidGenera}
-                    onChange={this.handleChange}
-                  />
-                </Label>
-              </div>
+                <div className='orchid-name'>
+                  <FieldBlock id='orchidGenera' value={fields.orchidGenera.value} onChange={form.handleInputChanges} label='Orchid Genera' isInvalid={fields.orchidGenera.isInvalid} feedback={fields.orchidGenera.msg} />
 
-              <div row={2} col={2}>
-                <Label>
-                  Orchid Species
-                  <Field
-                    valueKey='orchidSpecies'
-                    type='text'
-                    placeholder='Orchid Species'
-                    value={this.state.orchidSpecies}
-                    onChange={this.handleChange}
-                  />
-                </Label>
-              </div>
+                  <FieldBlock id='orchidSpecies' value={fields.orchidSpecies.value} onChange={form.handleInputChanges} label='Orchid Species' isInvalid={fields.orchidSpecies.isInvalid} feedback={fields.orchidSpecies.msg} />
+                </div>
 
-              <div row={3} col={1}>
-                <Label>
-                  Is it Fragrant?
-                  <Field
-                    valueKey='fragrance'
-                    groupName='fragrant'
-                    type='select'
-                    placeholder='Is it Fragrant?'
-                    value={this.state.fragrant}
-                    onChange={this.handleChange}
-                  >
-                    <option value='Yes'>Yes</option>
-                    <option value='No'>No</option>
-                    <option value='At Night'>At Night</option>
-                  </Field>
-                </Label>
-              </div>
+                <div className='care-container'>
+                  <div className='care-information'>
 
-              <div row={3} col={2}>
-                <Label>
-                  How often do you water?
-                  <Field
-                    valueKey='water'
-                    groupName='water'
-                    type='select'
-                    placeholder='How often do you water?'
-                    value={this.state.water}
-                    onChange={this.handleChange}
-                  >
-                    <option value='Daily'>Daily</option>
-                    <option value='Twice Daily'>Twice Daily</option>
-                    <option value='Once a Week'>Once a Week</option>
-                    <option value='As Needed'>As Needed</option>
-                  </Field>
-                </Label>
+                  </div>
+                </div>
+                <Dropzone />
               </div>
-
-              <div row={3} col={3}>
-                <Label>
-                  How much light
-                  <br />
-                  do you give your orchid?
-                  <Field
-                    valueKey='lightIntensity'
-                    groupName='lightIntensity'
-                    type='select'
-                    placeholder='How much light do you give your orchid?'
-                    value={this.state.lightIntensity}
-                    onChange={this.handleChange}
-                  >
-                    <option value='Low'>Low</option>
-                    <option value='Medium'>Medium</option>
-                    <option value='High'>High</option>
-                  </Field>
-                  <Field
-                    valueKey='lightType'
-                    groupName='lightType'
-                    type='select'
-                    placeholder='How much light do you give your orchid?'
-                    value={this.state.lightType}
-                    onChange={this.handleChange}
-                  >
-                    <option value='LED'>LED</option>
-                    <option value='T5'>T5</option>
-                    <option value='Natural Sun'>Natural Sun</option>
-                  </Field>
-                </Label>
-              </div>
-
-              <div row={4} col={1}>
-                <Label>
-                  At what temperature
-                  <br />
-                  do keep your orchid?
-                  <Field
-                    valueKey='temp'
-                    groupName='temp'
-                    type='select'
-                    placeholder='At what temperature do keep your orchid?'
-                    value={this.state.temp}
-                    onChange={this.handleChange}
-                  >
-                    <option value='Low'>Low</option>
-                    <option value='Medium'>Medium</option>
-                    <option value='High'>High</option>
-                  </Field>
-                </Label>
-              </div>
-
-              <div row={4} col={2}>
-                <Label>
-                  What level of humidity do give your orchid?
-                  <Field
-                    valueKey='humidity'
-                    groupName='humidity'
-                    type='select'
-                    placeholder='What level of humidity do give your orchid?'
-                    value={this.state.humidity}
-                    onChange={this.handleChange}
-                  >
-                    <option value='Low'>Low</option>
-                    <option value='Medium'>Medium</option>
-                    <option value='High'>High</option>
-                  </Field>
-                </Label>
-              </div>
-
-              <div row={5} col={2}>
-                <Dropzone
-                  value={this.state.images}
-                  onChange={this.handleChange}
-                />
-              </div>
-            <Button>Submit</Button>
-          </Form>
+              <button className='btn btn-primary'>Submit</button>
+            </form>
+            <div className='uploader-return'>
+              <h5>Orchid Name: {fields.orchidGenera.value} {fields.orchidSpecies.value}</h5>
+              <h5>Grower: {fields.firstName.value} {fields.lastName.value}</h5>
+            </div>
+          </div>
         </div>
-      </div>
-    );
-  }
+    )
 }
 
-export default Uploader;
+export default Uploader
