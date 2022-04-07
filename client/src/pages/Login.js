f7cb232f9d8a0e1062561b41edccdfdc8a15bcce
Ryan Howard <ryanghoward@protonmail.com>
2022-04-06 19:52:37 -0700

    Calling it a day

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -78,7 +78,6 @@ const Login = () => {
           <div className='container'>
             {/* <h1>Log In</h1> */}
             <form onSubmit={handleSubmit}>
-              {/* <label htmlFor='email'>Email:</label> */}
               <input
                 placeholder='   Email'
                 type='email'
@@ -89,7 +88,6 @@ const Login = () => {
                 value={email}
                 required
               />
-              {/* <label htmlFor='password'>Password:</label> */}
               <input
                 placeholder='   Password'
                 type='password'
@@ -104,11 +102,7 @@ const Login = () => {
               </div>
               <div className='signup-link'>
                 <p>
-                  Need an Account?
-                  {/* <br /> */}
-                  {/* <span className='line'> */}
-                  <a href='/signup'> Sign Up</a>
-                  {/* </span> */}
+                  Need an Account? <a href='/signup'>Sign Up</a>
                 </p>
               </div>
             </form>
