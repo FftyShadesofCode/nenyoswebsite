6855464a1fe920d703f05b66f37b421df484a32f
Ryan Howard <ryanghoward@protonmail.com>
2022-04-14 15:55:30 -0700

    Form built

--- client/src/components/gallery/UploadImage.js
+++ client/src/components/gallery/UploadImage.js
@@ -4,15 +4,22 @@ import { motion } from "framer-motion";
 import ProgressBar from "./ProgressBar";
 import graphic from "../../icons/image-files.svg";
 
+const initialValues = {
+  nickname: "",
+  species: "",
+  h20_freq: "",
+  light_conditions: "",
+  user_id: localStorage.id,
+};
+
 export default function UploadImage({ setReload }) {
   const type = ["image/jpeg", "image/jpg", "image/png", "image/svg"];
-
   const [values, setValues] = useState({
     file: "",
     formData: new FormData(),
     error: "",
   });
-
+  const [values2, setValues2] = useState(initialValues);
   const { file, formData, error } = values;
 
   const handleChange = (e) => {
@@ -32,6 +39,10 @@ export default function UploadImage({ setReload }) {
     }
   };
 
+  const handleChange2 = (e) => {
+    setValues2({ ...values2, [e.target.name]: e.target.value });
+  };
+
   return (
     <div>
       <form>
@@ -40,36 +51,66 @@ export default function UploadImage({ setReload }) {
         </div>
         <FormGroup className='formGroup'>
           <div className='username-input form-spacing'>
-            <label>
-              <input placeholder='Name/Nickname' name='nickname' type='text' />
-            </label>
+            <input
+              placeholder='Name/Nickname'
+              name='nickname'
+              type='text'
+              onChange={handleChange2}
+            />
+          </div>
+          <div className='password-input form-spacing'>
+            <input
+              placeholder='Species'
+              name='species'
+              type='text'
+              onChange={handleChange2}
+            />
+          </div>
+          <div className='password-input form-spacing'>
+            <input
+              placeholder='Water Frequency'
+              name='h20_freq'
+              type='text'
+              onChange={handleChange2}
+            />
+          </div>
+          <div className='password-input form-spacing'>
+            <input
+              placeholder='Light Conditions'
+              name='light_conditions'
+              type='text'
+              onChange={handleChange2}
+            />
+          </div>
+          <motion.img
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 2 }}
+            id='bold'
+            height='100'
+            width='100'
+            src={graphic}
+          />
+          <label>
+            <input
+              onChange={(e) => handleChange(e)}
+              type='file'
+              placeholder='Choose File'
+            />
+            {/* <span>Add</span> */}
+          </label>
+          {file && (
+            <ProgressBar
+              setReload={setReload}
+              values={values}
+              setValues={setValues}
+            />
+          )}
+          {error && <h1 className='error'>{error}</h1>}
+          <div className='submit'>
+            <Button id='submit-login'>Submit</Button>
           </div>
         </FormGroup>
-        <motion.img
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 2 }}
-          id='bold'
-          height='100'
-          width='100'
-          src={graphic}
-        />
-        <label>
-          <input
-            onChange={(e) => handleChange(e)}
-            type='file'
-            placeholder='Choose File'
-          />
-          <span>Add</span>
-        </label>
-        {file && (
-          <ProgressBar
-            setReload={setReload}
-            values={values}
-            setValues={setValues}
-          />
-        )}
-        {error && <h1 className='error'>{error}</h1>}
       </form>
     </div>
   );
