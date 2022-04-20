354812b3de2dde90c12f6f7b5a3b2c7150e8f568
Philip R McDavid <31264591+FftyShadesofCode@users.noreply.github.com>
2022-04-20 10:42:30 -0400

    Updating to React 18

--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import { AuthProvider } from "./context/AuthProvider";
-import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
-import store from "./store";
 
 import "../src/CSS Files/index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const root = ReactDOM.createRoot(
+    document.getElementById('root')
+)
+
+root.render(<App />)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
